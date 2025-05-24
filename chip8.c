6198a24af6fbf7ea5edcd39485e4417601394978
Khornne <maurice.pattinsonn@gmail.com>
2025-05-24 11:00:02 +0100

    Created Renderer

--- chip8.c
+++ chip8.c
@@ -1,11 +1,14 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_error.h>
 #include <SDL2/SDL_log.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_stdinc.h>
 #include <SDL2/SDL_video.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 SDL_Window *window;
+SDL_Renderer *renderer;
 
 int init_sdl(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -13,14 +16,20 @@ int init_sdl(void) {
   }
   SDL_Window *window =
       SDL_CreateWindow("CHIP-8 EMULATOR", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, 1000, 1000, 0);
+                       SDL_WINDOWPOS_CENTERED, 320, 240, 0);
   if (window == NULL) {
-    printf("Unable to create window: %s\n", SDL_GetError());
+    SDL_Log("Unable to create SDL Window: %s\n", SDL_GetError());
+    return 0;
   }
-  return 0;
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if (renderer == NULL) {
+    SDL_Log("Error unable to create SDL Renderer | %s\n", SDL_GetError());
+  }
+  return 1;
 }
 
 int final_cleanup(void) {
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
