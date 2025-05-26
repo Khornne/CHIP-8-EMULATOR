3ac05c8affeaccd02bd689420e6179186ccc3e38
Khornne <maurice.pattinsonn@gmail.com>
2025-05-26 15:27:02 +0100

    Addes use of Sructs

--- chip8.c
+++ chip8.c
@@ -7,37 +7,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-SDL_Window *window;
-SDL_Renderer *renderer;
+typedef struct {
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+} sdl_t;
 
-int init_sdl(void) {
+int init_sdl(sdl_t *sdl) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
     SDL_Log("Error Initisalising SLD %s\n", SDL_GetError());
+    return 1;
   }
-  SDL_Window *window =
-      SDL_CreateWindow("CHIP-8 EMULATOR", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, 320, 240, 0);
-  if (window == NULL) {
+  sdl->window = SDL_CreateWindow("CHIP-8 EMULATOR", SDL_WINDOWPOS_CENTERED,
+                                 SDL_WINDOWPOS_CENTERED, 320, 240, 0);
+  if (sdl->window == NULL) {
     SDL_Log("Unable to create SDL Window: %s\n", SDL_GetError());
     return 0;
   }
-  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  if (renderer == NULL) {
+  sdl->renderer = SDL_CreateRenderer(sdl->window, -1, SDL_RENDERER_ACCELERATED);
+  if (sdl->renderer == NULL) {
     SDL_Log("Error unable to create SDL Renderer | %s\n", SDL_GetError());
   }
   return 1;
 }
 
 int final_cleanup(void) {
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
+  sdl_t sdl;
+  SDL_DestroyRenderer(sdl.renderer);
+  SDL_DestroyWindow(sdl.window);
   SDL_Quit();
+  return 0;
+}
+
+// Initial Screen clearing it to background color
+int clear_window(void) {
+  sdl_t sdl;
+  SDL_RenderClear(sdl.renderer);
+  // SDL_GetRenderDrawColor(sdl.renderer, 255, 255, 255, 255);
   return 0;
 }
 
 int main(int argc, char *argv[]) {
   // SDL init
-  init_sdl();
+  sdl_t sdl = {0};
+  init_sdl(&sdl);
+
+  // Render Clear
+  clear_window();
 
   // Final Clean
   final_cleanup();
